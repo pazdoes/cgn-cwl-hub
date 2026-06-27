fb582a7bc85b9ea4a4c40e344dbe64092823fc93
0keks <zerokeks0x@gmail.com>
2026-06-27 10:37:37 +0100

    Fix share card: match SVG height to text line height

--- app/player/[tag]/page.js
+++ app/player/[tag]/page.js
@@ -161,8 +161,8 @@ function ShareCard({ data, latestOverall, rank, rankColour, avgEfficiency, avgDe
               <tbody>
                 <tr style={{ height: "28px" }}>
                   {rank <= 3 && (
-                    <td style={{ width: "28px", height: "28px", padding: 0, paddingRight: "8px", verticalAlign: "middle" }}>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke={rankColour} strokeWidth={1.8}>
+                    <td style={{ width: "34px", height: "28px", padding: 0, paddingRight: "8px", verticalAlign: "middle" }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 24 24" stroke={rankColour} strokeWidth={1.8}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"/>
                       </svg>
                     </td>
