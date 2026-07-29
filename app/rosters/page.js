806483718ea21be62dbd815345f9363584203922
0keks <zerokeks0x@gmail.com>
2026-07-29 12:15:52 +0100

    Rosters page: apply exact 1080x1920 image dimensions

--- app/rosters/page.js
+++ app/rosters/page.js
@@ -360,7 +360,7 @@ export default function RostersPage() {
   const BG = (
     <div className="absolute inset-0 pointer-events-none overflow-hidden">
       <div className="absolute top-[-200px] left-1/2 -translate-x-1/2 w-[100vw] max-w-[600px] h-[100vw] max-h-[600px] bg-purple-500/10 blur-3xl rounded-full"/>
-      <img src="/roster-bg.png" alt="" className="fixed inset-0 w-full h-full object-cover object-top opacity-20 select-none pointer-events-none"/>
+      <img src="/roster-bg.png" alt="" className="fixed top-0 left-1/2 -translate-x-1/2 opacity-20 select-none pointer-events-none" style={{width:"1080px",height:"1920px"}}/>
     </div>
   );
 
